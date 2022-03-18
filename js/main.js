2019e1b2224df8a68052652c90d31353182a58c3
faceman7353@gmail.com <faceman7353@gmail.com>
2022-03-18 18:10:05 +0900

    공통모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-const searchEl = document.querySelector('.search'); 
-// 위 내용은 클래스가 .search인 요소를 찾아서 searchEl에 저장하라 는 의미
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click',function(){
-    //Logic...
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder',' 통합검색');
-});
-
-searchInputEl.addEventListener('blur',function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder','');
-});
 
 
 
@@ -167,5 +150,3 @@ spyEls.forEach(function(spyEl){
 // const thisYear = document.querySelector(".this-year");
 // thisYear.textContent = new Date().getFullYear(); //2021
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
